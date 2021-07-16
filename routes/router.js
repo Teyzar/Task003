aec6f672479159cb59e556ebe8e7d2ae0fdb8514
Teyzar <thomasandrewzaragoza@gmail.com>
2021-07-16 21:43:18 +0800

    change token algho

--- routes/router.js
+++ routes/router.js
@@ -4,15 +4,26 @@ const users = require('../model/login');
 const router = express.Router();
 require('dotenv').config();
 const jwt = require('jsonwebtoken');
+const bc = require('bcrypt');
+const crypto = require('crypto');
 
 const user = process.env.DB_USER;
-const pass = process.env.DB_PASS;
+const secret = process.env.SECRET_KEY;
 
 router.use(express.json());
 
-router.get('/get' , async (req,res) => {
+var md5 = crypto.createHash('md5').update(user + secret).digest('hex');
+
+router.get('/get/' , async (req,res) => {
+    if (req.body.token === md5) {
     const phonedoc = await model.find();
     res.json(phonedoc);
+    } else {
+        res.json(400, {
+            error: 1,
+            msg: "invalid token"
+        });
+    }
 });
 
 router.post('/create', async(req, res) => {
@@ -46,30 +57,43 @@ router.delete('/delete/:id', async(req,res) => {
 })
 
 
-router.post('/login', async(req, res) => {
-    const account = new users(
-        {
-            username : req.body.username,
-            password : req.body.password,
-        });
-        if (account.username === user && account.password === pass) {
-            res.json({message: "Succesfuly logged in"});
+// router.post('/login', async(req, res) => {
+//     const account = new users(
+//         {
+//             username : req.body.username,
+//             password : req.body.password,
+//             token : req.body.token,
+//         });
+//        var md5 = crypto.createHash('md5').update(user + secret).digest('hex');
 
-            const token = jwt.sign(
-                { username: req.body.username },
-                'RANDOM_TOKEN_SECRET',
-                { expiresIn: '24h' });
-              res.status(200).json({
-                username: req.body.username,
-                password: req.body.password,
-                token: token
-              });
-        } else {
-              res.json(400, {
-                error: 1,
-                msg: "Invalid credentials"
-            });
-        }
-});
+//     //    if(req.body.token === md5){
+//     //     res.json(400, {
+//     //         error: 1,
+//     //         msg: "Invalid credentials"
+//     //     });
+//     //    }
+
+//         if (account.username === user && account.password === pass) {
+//             // const token = jwt.sign(
+//             //     { username: req.body.username},
+//             //     'TOKEN_SECRET',
+//             //     { expiresIn: '24h' });
+//             //   res.status(200).json({
+//             //     username: req.body.username,
+//             //     password: req.body.password,
+//             //     token: token
+//             //   });
+//             //   res.json({message: "Succesfuly logged in", token: md5});
+
+//             //res.json({token : req.body.token });
+//             res.json(md5);
+
+//         } else {
+//               res.json(400, {
+//                 error: 1,
+//                 msg: "Invalid credentials"
+//             });
+//         }
+// });
 
 module.exports = router;
